15fe2a71cfe4d314a0ce1cdd90f3bff4e6ddac2d
Miguel <miguel.acero-l@mail.escuelaing.edu.co>
2026-09-19 23:44:44 -0500

    chore: initialize maven structure

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# GW---api-gateway
+# GW - API GATEWAY
 GW - Single point of entry, routing, and cross-cutting controls orchestrator.
---
+++ src/main/java/co/edu/escuelaing/campusseguro/gateway/GatewayApplication.java
@@ -0,0 +1,10 @@
+package co.edu.escuelaing.campusseguro.gateway;
+
+/**
+ * Hello world!
+ */
+public class GatewayApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
